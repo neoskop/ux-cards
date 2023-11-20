37604d38b6c3b1831ec114faf84225fc15f853a2
Fritz Benning <benning@neoskop.de>
2023-11-20 16:57:14 +0000

    feat: add neoskop logo

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image'
 import { MainProps } from './types'
+import neoskopLogo from '@/assets/neoskop-logo.svg'
 import styles from './styles.module.scss'
 
 const Main = ({ children }: MainProps) => {
   return (
     <main className={styles.main}>
       <div className={styles.inner}>{children}</div>
+      <div className={styles.branding}>
+        <a href="https://neoskop.de" target="_blank">
+          <Image src={neoskopLogo} alt="Neoskop" width={150} height={42} />
+        </a>
+      </div>
     </main>
   )
 }
